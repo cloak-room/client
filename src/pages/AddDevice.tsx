48f12c3e17a58c2097dd832f48e7575e24aab72f
K Davison <kai@kdavison.dev>
2022-10-24 14:58:56 +1000

    Change Tab 1 to Add Device, remove empty css and generate tabs from json

--- src/pages/AddDevice.tsx
+++ src/pages/AddDevice.tsx
@@ -1,19 +1,18 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Add Device</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Add Device</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
