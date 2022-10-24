c1104a3d3b5b9f693d354b1db91b8669e98ec448
K Davison <kai@kdavison.dev>
2022-10-24 21:24:53 +1000

    Start Login Page - add state variables, inputs and functions for calling the api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,52 @@
 import {
+  IonButton,
   IonContent,
   IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
+  IonRadio,
+  IonRadioGroup,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer/";
+import React, { useState } from "react";
+import useData from "../utils/useData";
+
+async function getUsers() {
+  const url = `${process.env.API ?? ""}/getUsers`;
+  const response = await fetch(url);
+  console.log(response);
+
+  console.log();
+
+  // const data: any[] = await response.json();
+  const data = Array.from({ length: 10 }, (_, k) => ({
+    username: `User ${k + 1}`,
+  }));
+  console.log(data);
+
+  return data;
+}
 
 export default function LoginPage() {
+  const { data: users, loading, error } = useData(getUsers);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>(""); // For later
+
+  const handleLogin = async (event: React.MouseEvent<HTMLIonButtonElement>) => {
+    const url = `${process.env.API ?? ""}/login`;
+    const args = { username }; // Implement password later
+    console.log("send", args);
+
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(args),
+    });
+    console.log(response);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,10 +57,32 @@ export default function LoginPage() {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Login</IonTitle>
+            <IonTitle size="large">Add Device</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Login page" />
+        <main>
+          <IonList>
+            <IonRadioGroup
+              value={username}
+              onIonChange={(event: Event) =>
+                setUsername((event.target as HTMLInputElement).value)
+              }
+              placeholder="Select username"
+            >
+              {users.map((user) => (
+                <IonItem>
+                  <IonLabel>{user.username}</IonLabel>
+                  <IonRadio
+                    slot="end"
+                    key={user.username}
+                    value={user.username}
+                  ></IonRadio>
+                </IonItem>
+              ))}
+            </IonRadioGroup>
+            <IonButton onClick={handleLogin}>Login</IonButton>
+          </IonList>
+        </main>
       </IonContent>
     </IonPage>
   );
