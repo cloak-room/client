84c11d36dddb2ab0506f2ae64937a56765048e3a
K Davison <kai@kdavison.dev>
2022-10-24 15:15:22 +1000

    Explore Container - refactor and put in folder

---
+++ src/components/ExploreContainer/index.tsx
@@ -0,0 +1,23 @@
+import "./ExploreContainer.css";
+
+interface ContainerProps {
+  name: string;
+}
+
+export default function ExploreContainer({ name }: ContainerProps) {
+  return (
+    <div className="container">
+      <strong>{name}</strong>
+      <p>
+        Explore{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
+    </div>
+  );
+}
