839cc25a6ed79b53ac3ee57f8f0c0bc338f6cbbb
K Davison <kai@kdavison.dev>
2022-10-24 15:19:35 +1000

    fix explore container imports

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,7 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
+import ExploreContainer from "../components/ExploreContainer/";
 
 export default function LoginPage() {
   return (
