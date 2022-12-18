202f6c75ffd7c2130688e78bf7fb54028ec276e8
benjaminARowe <benjamin.alan.rowe@gmail.com>
2022-12-18 20:59:42 +1000

    add confirmation to add screen

--- src/pages/AddDevice.tsx
+++ src/pages/AddDevice.tsx
@@ -9,6 +9,7 @@ import {
   IonSelect,
   IonSelectOption,
   useIonToast,
+  useIonAlert,
 } from "@ionic/react";
 import React, { useState } from "react";
 import { apiUrl } from "../App";
@@ -22,16 +23,11 @@ export default function AddDevicePage() {
   const { data: paymentMethods } = usePaymentMethods();
   const [presentToast] = useIonToast();
 
+  const [presentAlert] = useIonAlert();
+
   const { user } = useUserCtx();
 
   const inputs = [
-    {
-      key: "userID",
-      hidden: true,
-      label: "Cashier",
-      placeholder: "Username of cashier adding the device",
-      state: useState<number>(user?.id ?? 0),
-    },
     {
       key: "ownerName",
       label: "Client Name",
@@ -87,44 +83,67 @@ export default function AddDevicePage() {
   ) => {
     const url = `${apiUrl}/items/add`;
     let args = Object.assign(
-      {},
+      { userId: user?.id },
       ...inputs.map((input) => ({ [input.key]: input.state[0] }))
     );
     console.log("send", args);
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(args),
-    });
-
-    console.log("response", response);
-    const { error, message } = await response.json();
-
-    // If item added successfully
-    if (!error) {
-      // Clear inputs
-      inputs.forEach((input) => {
-        const [value, setValue] = input.state;
-        const inputType = typeof value;
-        const defaultValue: any = inputType === "string" ? "" : 0;
-        setValue(defaultValue);
-      });
-    }
-
-    // Error or success message using message from the backend
-    presentToast({
-      message: `${message}`,
-      color: error ? "danger" : "success",
-      position: "top",
-      duration: 3000,
+    console.log(itemTypes);
+    presentAlert({
+      header: `Please Collect ${
+        paymentMethods
+          ? paymentMethods.find((x) => x.id === args.paymentMethod)?.name
+          : ""
+      } payment of $${
+        (itemTypes?.find((x) => x.id === args.itemTypeID)?.price ?? 500) / 100
+      } from ${args.ownerName}`,
+      cssClass: "custom-alert",
       buttons: [
         {
-          text: "Dismiss",
-          role: "cancel",
+          text: "Cancel",
+          cssClass: "alert-button-cancel",
+          handler: () => true,
+        },
+        {
+          text: "Confirm",
+          cssClass: "alert-button-confirm",
+          handler: async () => {
+            const response = await fetch(url, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+              },
+              body: JSON.stringify(args),
+            });
+
+            console.log("response", response);
+            const { error, message } = await response.json();
+
+            // If item added successfully
+            if (!error) {
+              // Clear inputs
+              inputs.forEach((input) => {
+                const [value, setValue] = input.state;
+                const inputType = typeof value;
+                const defaultValue: any = inputType === "string" ? "" : 0;
+                setValue(defaultValue);
+              });
+            }
+
+            // Error or success message using message from the backend
+            presentToast({
+              message: `${message}`,
+              color: error ? "danger" : "success",
+              position: "top",
+              duration: 3000,
+              buttons: [
+                {
+                  text: "Dismiss",
+                  role: "cancel",
+                },
+              ],
+            });
+          },
         },
       ],
     });
